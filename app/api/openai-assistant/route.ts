d0fd8dd2869a7c5b172a5d9b326324698ae0d3f5
Stephen Walther <Stephen.Walther@superexpert.com>
2024-03-24 14:51:42 -0700

    Update config for Edge Functions

--- app/api/openai-assistant/route.ts
+++ app/api/openai-assistant/route.ts
@@ -3,9 +3,8 @@ import OpenAI from 'openai'
 
 // this enables Edge Functions in Vercel
 // see https://vercel.com/blog/gpt-3-app-next-js-vercel-edge-functions
-export const config = {
-    runtime: "edge",
-};
+// and updated here: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+export const runtime = "edge";
 
 // post a new message and stream OpenAI Assistant response
 export async function POST(request:NextRequest) {
